d6b473a0c0cb5d28430fa66b7006f93e9c5b396b
Azure <847896757@qq.com>
2024-03-09 23:43:41 +0000

    feedbacks adds

--- application/views/feedback_views.py
+++ application/views/feedback_views.py
@@ -47,14 +47,14 @@ return the feedbacks list for the user
 @login_required
 def get_feedbacks_by_user_view(request, user_id):
     feedbacks = feedback_service.get_feedbacks_by_user(user_id)
-    feedbacks_data = list(feedbacks.values('user','job__title','rate','comment','date','job_id'))
+    feedbacks_data = list(feedbacks.values('user','job__title','rate','comment','date','job_id','user__user_name'))
     return JsonResponse({'feedbacks': feedbacks_data})
 
 @require_http_methods(["GET"])
 @login_required
 def get_feedbacks_by_job_view(request, job_id):
     feedbacks = feedback_service.get_feedbacks_by_job(job_id)
-    feedbacks_data = list(feedbacks.values('user','job__title','rate','comment','date','job_id'))
+    feedbacks_data = list(feedbacks.values('user','job__title','rate','comment','date','user__user_name'))
     return JsonResponse({'feedbacks': feedbacks_data})
     
     
